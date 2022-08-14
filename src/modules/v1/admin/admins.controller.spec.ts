7cb3efb7111e4c8d1dbfb341f88628fa228070c5
Seyyed Morteza SeyyedAghaei <sseedd524@gmail.com>
2022-08-15 01:35:44 +0430

    admins test

--- src/modules/v1/admin/admins.controller.spec.ts
+++ src/modules/v1/admin/admins.controller.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import AdminsController from './admins.controller';
 import AdminsService from './admins.service';
+import { MockType } from '@v1/types';
 
 describe('Admins Controller', () => {
   let controller: AdminsController;
+  let service: MockType<AdminsService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,17 +13,40 @@ describe('Admins Controller', () => {
       providers: [
         {
           provide: AdminsService,
-          useValue: {},
+          useValue: {
+            create: jest.fn(),
+            getById: jest.fn(),
+            update: jest.fn(),
+            getAllWithPagination: jest.fn(),
+          },
         },
       ],
     }).compile();
 
-    controller = module.get<AdminsController>(
-      AdminsController,
-    );
+    controller = module.get<AdminsController>(AdminsController);
+    service = module.get(AdminsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create', async () => {
+    expect(controller.create).toBeDefined();
+    await controller.create({} as any);
+    expect(service.create).toBeCalled();
+  });
+
+  it('should update', async () => {
+    expect(controller.update).toBeDefined();
+    await controller.update('', {} as any);
+    expect(service.update).toBeCalled();
+  });
+
+  it('should getAllWithPagination', async () => {
+    expect(controller.getAll).toBeDefined();
+    service.getAllWithPagination.mockReturnValueOnce({});
+    await controller.getAll({} as any);
+    expect(service.getAllWithPagination).toBeCalled();
   });
 });
--- src/modules/v1/admin/admins.service.spec.ts
+++ src/modules/v1/admin/admins.service.spec.ts
@@ -1,23 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import AdminsService from './admins.service';
+import AdminsRepository from '@v1/admin/admins.repository';
+import { MockType } from '@v1/types';
 
 describe('AdminsService', () => {
   let service: AdminsService;
+  let repository: MockType<AdminsRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
-          provide: AdminsService,
-          useValue: {},
+          provide: AdminsRepository,
+          useValue: {
+            create: jest.fn(),
+            getById: jest.fn(),
+            updateById: jest.fn(),
+            getAllWithPagination: jest.fn(),
+          },
         },
+        AdminsService,
       ],
     }).compile();
 
     service = module.get<AdminsService>(AdminsService);
+    repository = module.get(AdminsRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create', async () => {
+    expect(service.create).toBeDefined();
+    await service.create({} as any);
+    expect(repository.create).toBeCalled();
+  });
+
+  it('should getById', async () => {
+    expect(service.getById).toBeDefined();
+    await service.getById({} as any);
+    expect(repository.getById).toBeCalled();
+  });
+
+  it('should update', async () => {
+    expect(service.update).toBeDefined();
+    await service.update('', {} as any);
+    expect(repository.updateById).toBeCalled();
+  });
+
+  it('should getAllWithPagination', async () => {
+    expect(service.getAllWithPagination).toBeDefined();
+    await service.getAllWithPagination({} as any);
+    expect(repository.getAllWithPagination).toBeCalled();
   });
 });
