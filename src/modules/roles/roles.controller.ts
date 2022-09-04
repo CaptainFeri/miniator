6c371ac420888636de219cf7a895a2f81e14fb36
Seyyed Morteza SeyyedAghaei <sseedd524@gmail.com>
2022-09-05 03:59:24 +0430

    test and fix roles grpc

--- src/modules/roles/roles.controller.ts
+++ src/modules/roles/roles.controller.ts
@@ -3,13 +3,9 @@ import {
   Controller,
   NotFoundException,
   UseGuards,
-  UseInterceptors,
 } from '@nestjs/common';
 import JwtAccessGuard from '@guards/jwt-access.guard';
-import WrapResponseInterceptor from '@interceptors/wrap-response.interceptor';
 import { PaginationParamsInterface } from '@interfaces/pagination-params.interface';
-import { SuccessResponseInterface } from '@interfaces/success-response.interface';
-import ResponseUtils from '@utils/response.utils';
 import PaginationUtils from '@utils/pagination.utils';
 import RolesService from './roles.service';
 import User from '@decorators/user.decorator';
@@ -19,7 +15,6 @@ import { Public } from '@decorators/public.decorator';
 import CreateRoleDto from './dto/create-role.dto';
 import { GrpcMethod } from '@nestjs/microservices';
 
-@UseInterceptors(WrapResponseInterceptor)
 @Controller()
 export default class RolesController {
   constructor(private readonly rolesService: RolesService) {}
@@ -27,12 +22,7 @@ export default class RolesController {
   @Types(TypesEnum.superAdmin)
   @GrpcMethod('RolesService', 'Create')
   async create(data: CreateRoleDto): Promise<any> {
-    const role = await this.rolesService.create(data);
-
-    return ResponseUtils.success('roles', {
-      message: 'Success',
-      role,
-    });
+    return await this.rolesService.create(data);
   }
 
   @Public()
@@ -48,23 +38,22 @@ export default class RolesController {
       paginationParams,
     );
 
-    return ResponseUtils.success('roles', paginatedRoles.paginatedResult, {
-      location: 'roles',
-      paginationParams,
+    return {
+      roles: paginatedRoles.paginatedResult,
       totalCount: paginatedRoles.totalCount,
-    });
+    };
   }
 
   @Public()
-  @GrpcMethod('RolesService', 'GetItem')
-  async getById(query: any): Promise<SuccessResponseInterface> {
+  @GrpcMethod('RolesService', 'GetById')
+  async getById(query: any) {
     const foundRole = await this.rolesService.getById(query.id);
 
     if (!foundRole) {
       throw new NotFoundException('The role does not exist');
     }
 
-    return ResponseUtils.success('roles', foundRole);
+    return foundRole;
   }
 
   @UseGuards(JwtAccessGuard)
@@ -73,25 +62,27 @@ export default class RolesController {
   async update(body: any): Promise<any> {
     await this.rolesService.update(body.id, body);
 
-    return ResponseUtils.success('roles', {
-      message: 'Success!',
-    });
+    return {
+      success: true,
+    };
   }
 
   @GrpcMethod('RolesService', 'Request')
   async request(body: any, @User() account: AccountEntity): Promise<any> {
     await this.rolesService.request(body.id, account.id);
-    return ResponseUtils.success('roleRequests', {
-      message: 'Success!',
-    });
+
+    return {
+      success: true,
+    };
   }
 
   @GrpcMethod('WalletService', 'Accept')
   @Types(TypesEnum.superAdmin, TypesEnum.admin)
   async accept(body: any, @User() account: AccountEntity): Promise<any> {
     await this.rolesService.accept(body.id, account);
-    return ResponseUtils.success('roleRequests', {
-      message: 'Success!',
-    });
+
+    return {
+      success: true,
+    };
   }
 }
