32ff30a2e6568aba92b9455f0042919ac179f3ff
ferriloper <mh.hsp1377@gmail.com>
2022-11-13 08:56:47 +0330

    implement dynamic enviroment

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,28 @@
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('miniator');
+  const configService = app.get(ConfigService);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      stopAtFirstError: true,
+    }),
+  );
+  const port = configService.get('port');
+  await app.listen(port || 3000);
+  logger.log(`Application listening on port: ${port}`);
 }
 bootstrap();
