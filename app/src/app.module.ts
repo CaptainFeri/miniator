2894535bdf00cd09f22dd025b665b7a8bec02dfb
ferriloper <mh.hsp1377@gmail.com>
2022-11-13 09:14:39 +0330

    impelemnt typeorm module type postgres

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,14 +1,34 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService, ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import appEnvVonfig from './config/app-env.vonfig';
+import appEnvConfig from './config/app-env.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appEnvVonfig],
+      load: [appEnvConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (
+        configService: ConfigService<ConfigType<typeof appEnvConfig>>,
+      ) => {
+        const postgresConfig = configService.get('postgres', { infer: true });
+        return {
+          type: 'postgres',
+          host: postgresConfig.url,
+          port: +postgresConfig.port,
+          username: postgresConfig.username,
+          password: postgresConfig.password,
+          database: postgresConfig.dbname,
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          migrations: ['dist/migrations/**/*{.ts,.js}'],
+          migrationsRun: true,
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
